1a1c37fe3cda81512719ff7ad586983ead134b5f
fabricio4077 <orejona2501>
2018-11-12 11:16:35 -0500

    ordenamiento en competencia

--- grails-app/controllers/docentes/CompetenciaController.groovy
+++ grails-app/controllers/docentes/CompetenciaController.groovy
@@ -28,6 +28,8 @@ class CompetenciaController extends Shield {
         params = params.clone()
         params.max = params.max ? Math.min(params.max.toInteger(), 100) : 10
         params.offset = params.offset ?: 0
+        params.sort = 'carrera'
+        params.order = 'asc'
         if(all) {
             params.remove("max")
             params.remove("offset")
